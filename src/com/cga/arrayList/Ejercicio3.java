c91c366146f2abc7366f853d5776a9f83ac563ea
Juan <anonimox839@gmail.com>
2023-08-28 20:55:08 +0100

    Actulizacion

--- src/com/cga/arrayList/Ejercicio3.java
+++ src/com/cga/arrayList/Ejercicio3.java
@@ -8,12 +8,15 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         
         boolean condicion = true;
+        char c = ' ';
         ArrayList<DatosPersona> personas = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         do {
 
             System.out.println("Deseas Registrar Datos? \n Si = \"S\" o \"s\"     No = \"N\" o \"n\" ");
+            c = sc.next().charAt(c);
+            
             
         } while (condicion);
 
